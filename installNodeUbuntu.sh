62557b8eb1708c4430794ea1ab48336f1ee39869
lowkeyop <ckennerly9@gmail.com>
2017-06-04 15:38:53 -0400

    Added successful node installation check
    Checking whether nodejs  was successfully installed before creating a
    symbolic link.  Changed wording.

--- installNodeUbuntu.sh
+++ installNodeUbuntu.sh
@@ -12,8 +12,11 @@ echo "checking if node binary exists.."
 if[ -e /usr/bin/node ]
 then
 	echo "Node binaries exist"
-else
-	echo "Create a node symlink using nodejs binaries."
+elif [ -e /usr/bin/nodejs]
+then
+	echo "Creating a node symlink using nodejs binaries."
 	sudo ln -s /usr/bin/nodejs /usr/bin/node
 	echo "To run your angular app, navigate inside the directory, then type the following: \n'sudo npm install && sudo npm start"
+else
+	echo "unable to locate necessary binaries to create default node binary"
 fi
